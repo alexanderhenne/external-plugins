76218041a76e4c66d5d05648616ff2915ed38464
Lotto <prozet@live.se>
2020-04-13 19:10:44 +0200

    Add Monkey Metrics plugin

--- src/test/java/com/monkeymetrics/MonkeyMetricsPluginTest.java
+++ src/test/java/com/monkeymetrics/MonkeyMetricsPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.monkeymetrics;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class MonkeyMetricsPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(MonkeyMetricsPlugin.class);
 		RuneLite.main(args);
 	}
 }
